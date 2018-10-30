6e7fc0ed344ce5b0d88e904e8eb22d505d93635d
Hansuk Hong <flavono123@gmail.com>
2018-10-30 23:22:01 +0900

    yarn add semantic-ui-react
    import semantic-ui-css from cdn
    test with rendering a simple button
    
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <Button primary>Learn React</Button>
           </a>
         </header>
       </div>
